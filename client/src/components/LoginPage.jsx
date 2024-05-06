c9961f3557e5be4c6242d13af49390f939ab42bb
Vanessa Kasminoff <sunnset07@gmail.com>
2024-05-05 22:14:29 -0700

    Added hunter logo to login page and modified multiple style elements

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import HunterLogo from '../assets/images/hunterxhunterlogotransparent.png'
 
 const LoginPage = () => {
     const [credentials, setCredentials] = useState({
@@ -17,73 +18,39 @@ const LoginPage = () => {
 
     return (
         <div id="login-container">
-                <h1>HUNTER'S TAVERN</h1>
-                <Form onSubmit={handleSubmit}>
+                <img className="hunter-logo" src={HunterLogo} alt="Hunter x Hunter Logo"/>
+                <Form onSubmit={handleSubmit} className="form-container">
                     <Form.Group className="mb-3" controlId="name">
-                        <Form.Label>Name</Form.Label>
+                        <Form.Label column="lg" className="form-label">Name</Form.Label>
                         <Form.Control 
                             type="text"            
                             name="name"
-                            placeholder="Type name here..."
                             value={credentials.name}
                             onChange={e => setCredentials({...credentials, name: e.target.value})}
                             required
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="password">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label column="lg" className="form-label">Password</Form.Label>
                         <Form.Control
                             type="password"
                             name="password"
-                            placeholder="Type password here..."
                             value={credentials.password}
                             onChange={e => setCredentials({...credentials, password: e.target.value})}
                             required
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="license">
-                        <Form.Label>Hunter License #</Form.Label>
+                        <Form.Label column="lg" className="form-label">Hunter License #</Form.Label>
                         <Form.Control
                             type="text"
                             name="license"
-                            placeholder="Type license here..."
                             value={credentials.license}
                             onChange={e => setCredentials({...credentials, license: e.target.value})}
                             required
                         />
                     </Form.Group>
-                    <Button as="input" type="submit" value="Login"/>
-                    {/* <label htmlFor="name">Name</label>
-                    <input 
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={credentials.name}
-                        onChange={e => setCredentials({...credentials, name: e.target.value})}
-                        required
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={e => setCredentials({...credentials, password: e.target.value})}
-                        required
-                    />
-                    <label htmlFor="license">Hunter License #</label>
-                    <input
-                        type="text"
-                        id="license"
-                        name="license"
-                        value={credentials.license}
-                        onChange={e => setCredentials({...credentials, license: e.target.value})}
-                        required
-                    /> */}
-                    {/* <input
-                        type="submit"
-                        value="Login"
-                    /> */}
+                    <Button as="input" type="submit" value="Login" className="form-button"/>
                 </Form>
         </div>
     )
